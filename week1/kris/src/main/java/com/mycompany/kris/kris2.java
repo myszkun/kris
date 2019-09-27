9a8337ce3632ce448a54af025d977ff2a6392482
Krzysztof Lesniewski <4lesnk72@solent.ac.uk>
2019-09-27 10:28:08 +0100

    git ignore

---
+++ week1/kris/src/main/java/com/mycompany/kris/kris2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.kris;
+
+/**
+ *
+ * @author 4lesnk72
+ */
+public class kris2 {
+    
+}
